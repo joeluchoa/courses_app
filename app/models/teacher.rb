a07251c4ef75bc3ba06afd5178548863a0873328
Joel Uchoa <joeluchoa@gmail.com>
2025-08-22 23:16:45 +0200

    Allow Teacher with empty phone and empty email.

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,5 +2,5 @@ class Teacher < ApplicationRecord
   has_many :courses, dependent: :restrict_with_error
 
   validates :name, presence: true
-  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  #validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
