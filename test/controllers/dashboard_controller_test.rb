b82ae5a448c946209a720acc2869a765741ee6a9
Joel Uchoa <joeluchoa@gmail.com>
2026-02-20 23:57:11 +0100

    Add student and course links to dashboard activity list

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -8,8 +8,15 @@ class DashboardControllerTest < ActionDispatch::IntegrationTest
     sign_in @user, scope: :user
   end
 
-  test "should get index" do
+  test "should get index and show activity links" do
+    create(:attendance) if Attendance.count == 0
+    attendance = Attendance.first
+    
     get dashboard_url(locale: :en)
     assert_response :success
+    assert_select "div.activity-list" do
+      assert_select "a[href=?]", student_path(attendance.student, locale: :en), text: attendance.student.full_name
+      assert_select "a[href=?]", course_path(attendance.course, locale: :en), text: attendance.course.name
+    end
   end
 end
